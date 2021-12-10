304365e067dd4d296e7f15d4d440dfb3fb8df838
lorber98 <95907752+lorber98@users.noreply.github.com>
2021-12-10 11:35:04 +0100

    Add files via upload

---
+++ matematica.py
@@ -0,0 +1,22 @@
+import math
+
+r=12+5j
+a=5
+b=2
+
+
+print(r, type(r))
+print("Parte reale:",r.real,type(r.real))
+print("Parte immaginaria:",r.imag, type(r.imag))
+print("---------------------------------")
+print(a, type(a))
+print(b, type(b))
+print("---------------------------------")
+for i in range(0,5,1):
+	print("i=",i)
+	if (i == b and a<10):
+		print("Find b:",i)
+print("---------------------------------")
+print("sin(b)="math.sin(a))
+print("---------------------------------")
+
